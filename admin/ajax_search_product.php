492fe02eaefe393fea539b327a346cf594526b9d
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2019-09-23 20:18:03 -0700

    Updated ajax_search_product.php.
    Added wildcard to search item, which would allow to seach database using keywords.

--- admin/ajax_search_product.php
+++ admin/ajax_search_product.php
@@ -3,12 +3,14 @@ include('includes/header.php');
 require('includes/pdocon.php');
 $raw_form_data=cleandata($_POST['productname']);
 $clean_form_data=sanitizer($raw_form_data);
+$clean_form_data='%'.$clean_form_data.'%';
 $db = new Pdocon;
-$db->query("SELECT * FROM inventory WHERE productName=:name");
+$db->query("SELECT * FROM inventory WHERE productName LIKE :name");
 $db->bindvalue(':name',$clean_form_data,PDO::PARAM_STR); 
 $db->execute();
-$row = $db->fetchSingle();
-if($row){ 
+$results = $db->fetchMultiple();
+if($results){ 
+    foreach ($results as $result){
    echo ' <table class="table table-bordered table-hover text-center">
     <thead >
       <tr>
@@ -24,19 +26,20 @@ if($row){
     </thead>
     <tbody>
       <tr>
-        <td>'.$row['id'].'</td>
-        <td>'.$row['productName'].'</td>
-        <td>'.$row['productDescription'].'</td>
-        <td>'.$row['productSupplier'].'</td>
-        <td>'.$row['productEmail'].'</td>
-        <td>'.$row['productCost'].'</td>
-        <td>'.$row['quantity'].'</td>
-        <td><img src="uploaded_image/'. $row['image'] .'"style="width:100px;height:100px"></td>
-        <td><a href="reports.php?report_id='.$row['id'].'class="btn btn-primary">View</a></td>
-        <td><a href="edit_product.php?product_id='.$row['id'].'" class="btn btn-danger">Edit</a></td> 
+        <td>'.$result['id'].'</td>
+        <td>'.$result['productName'].'</td>
+        <td>'.$result['productDescription'].'</td>
+        <td>'.$result['productSupplier'].'</td>
+        <td>'.$result['productEmail'].'</td>
+        <td>'.$result['productCost'].'</td>
+        <td>'.$result['quantity'].'</td>
+        <td><img src="uploaded_image/'. $result['image'] .'"style="width:100px;height:100px"></td>
+        <td><a href="reports.php?report_id='.$result['id'].'class="btn btn-primary">View</a></td>
+        <td><a href="edit_product.php?product_id='.$result['id'].'" class="btn btn-danger">Edit</a></td> 
       </tr>
     </tbody>
  </table>';
+    }
 }else{
 echo "<p class='bg-success text-center' style='font-weight:bold;'>No results found.</p>";
 }
